a4365ff188ea6d1f253f0f6871e9084b69880b80
Dan Farrow <dan@zellement.com>
2019-12-05 19:14:44 +0000

    Add roboto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
 module.exports = {
   theme: {
     extend: {
+      colors: {
+        blue: {
+          dark: '#26486E',
+          mid: '#81AEDD',
+        },
+        grey: {
+          light: '#eee',
+          standard: '#444444',
+        },
+        yellow: '#F8F4B3',
+        brown: '#8A8963',
+      },
       fontFamily: {
-        mono: [
-          'IBM Plex Mono',
-          'Menlo',
-          'Monaco',
-          'Consolas',
-          '"Liberation Mono"',
-          '"Courier New"',
-          'monospace',
+        sans: [
+          'Roboto',
+          'sans-serif'
         ],
       },
     }
