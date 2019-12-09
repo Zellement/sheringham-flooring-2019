77e65c6bea65bb18c175a5427de9663b2eea5c4b
Dan Farrow <dan@zellement.com>
2019-12-09 22:54:09 +0000

    Images and index start

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,41 +24,62 @@ const item = {
   },
 }
 
-const IndexPage = ({data}) => {
+const IndexPage = ({ data }) => {
 
   const post = data.datoCmsHomePage
 
   return (
     <>
-      <SEO title="Home" />
+      <SEO title="Carpets, Wooden Flooring, Vinyl Flooring Cromer, Fakenham, North Walsham" />
+
       <motion.section
         variants={container}
-        initial="hidden" 
+        initial="hidden"
         animate="visible"
-        className="container"
+        className=""
       >
-        <motion.div 
+
+        <motion.div
+          className="relative"
+          variants={item}
+          transition="easeInOut"
+        >
+
+          <div className="diagonal-stripes-grey absolute -z-10 w-3/5"></div>
+
+          <div className="container ">
+
+            <div className="text-center p-4 md:text-left md:w-3/5">
+              <p className="hero__bold">{post.heroBold}</p>
+              <p className="hero__regular">{post.heroRegular}</p>
+              <p className="hero__small">{post.heroSmall}</p>
+
+            </div>
+
+          </div>
+
+          <div className="md:w-1/2 md:absolute md:right-0 md:top-0 md:z-10 md:mt-4">
+
+            <Img
+              className=""
+              fluid={post.mainHeroImage.fluid}
+            />
+
+          </div>
+
+        </motion.div>
+
+        <motion.div
           className="content"
           variants={item}
           transition="easeInOut"
         >
-          <h1>{post.heroBold}</h1>
-          <h2>{post.heroRegular}</h2>
-          <h3>{post.heroSmall}</h3>
 
-          <Img fluid={post.mainHeroImage.fluid} />
+          sdasd
 
         </motion.div>
 
-        <motion.div 
-          className="content"
-          variants={item}
-          transition="easeInOut"
-        >
-          <hr className="block my-8" />
-        </motion.div>
-
-        <motion.div 
+        <motion.div
           className="content"
           variants={item}
           transition="easeInOut"
@@ -66,7 +87,7 @@ const IndexPage = ({data}) => {
           <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
           <h2>Lorem ipsum dolor sit amet</h2>
-          
+
           <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </motion.div>
       </motion.section>
@@ -83,8 +104,8 @@ query MyQuery {
     heroBold
     heroSmall
     mainHeroImage {
-      fluid(maxHeight: 200, maxWidth: 1800) {
-        ...GatsbyDatoCmsFluid
+      fluid(imgixParams: {h: "350", w: "800"}) {
+        ...GatsbyDatoCmsFluid_tracedSVG
       }
     }
   }
