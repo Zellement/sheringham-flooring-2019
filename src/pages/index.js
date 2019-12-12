984f047808272c8dd86e2b54b9323dd3ff37e1ea
Dan Farrow <dan@zellement.com>
2019-12-12 22:59:37 +0000

    Add in copy and html content

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Img from "gatsby-image"
 import { Link } from "gatsby"
 // import { HTMLContent } from "../components/content"
 import Hero from "../components/hero"
+import { HTMLContent } from "../components/content"
 
 const duration = 0.35
 
@@ -79,15 +80,13 @@ const IndexPage = ({ data }) => {
         </motion.div>
 
         <motion.div
-          className="content"
+          className="container"
           variants={item}
           transition="easeInOut"
         >
-          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          
+          <HTMLContent content={post.copy} />
 
-          <h2>Lorem ipsum dolor sit amet</h2>
-
-          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </motion.div>
       </motion.section>
     </>
