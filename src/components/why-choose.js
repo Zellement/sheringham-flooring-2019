b9711b41e37bfc9a7b40075ba70603a78e5ff15c
Dan Farrow <dan@zellement.com>
2019-12-15 18:54:11 +0000

    Update type and why choose

--- src/components/why-choose.js
+++ src/components/why-choose.js
@@ -29,16 +29,16 @@ const WhyChooseUs = ( {containerClass} ) => {
         
         {data.allDatoCmsWhyChoose.edges.map(whyChooseData => (
 
-            <div className={"p-8 flex flex-col " + containerClass} key={whyChooseData.node.id}>
+            <div className={"max-w-6xl mx-auto p-8 flex flex-col " + containerClass} key={whyChooseData.node.id}>
 
                 {whyChooseData.node.reasons.map(reasonData => (
 
-                    <div className="text-center py-4 px-2" key={reasonData.id}>
+                    <div className="text-center py-4 px-2 flex-1" key={reasonData.id}>
                         
                         <div className={reasonData.backgroundClass + " w-16 p-4 rounded-lg block mx-auto mb-3"}>
-                            <img className="w-full" src={reasonData.icon.url} />
+                            <img alt={reasonData.title} className="w-full" src={reasonData.icon.url} />
                         </div>
-                        <span className="block text-xl font-bold text-blue-dark">{reasonData.title}</span>
+                        <span className="block text-2xl font-bold text-blue-dark">{reasonData.title}</span>
                         <span className="block text-lg text-blue-dark">{reasonData.blurb}</span>
                         
                     </div>
